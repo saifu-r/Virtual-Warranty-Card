941c65c6d1257b203d2b8fee770f345f9432687b
Saifur Rahman Rubayet <saifur.rubayet@northsouth.edu>
2024-10-22 00:53:27 +0600

    dataset service methods formed

---
+++ src/app/services/dataset.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+export interface Dataset{
+  store?: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatasetService {
+
+  constructor() { }
+
+  async get(singleDataset: Dataset) {
+    let getKeys = Object.keys(singleDataset);
+    let data = null;
+    if (getKeys.length > 0) {
+      let jsonData = await require("./../datasets/" + getKeys[0] + ".json");
+      data = jsonData;
+    }
+    return this.responseFormat(data);
+  }
+
+  private responseFormat(data: any) {
+    let responseFormat = {
+      success: data != null ? true : false,
+      data: data
+    };
+    return responseFormat;
+  }
+
+
+}
