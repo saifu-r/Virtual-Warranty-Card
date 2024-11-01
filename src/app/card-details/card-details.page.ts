7c887f2712d6ea1071640400b24303c20dc8e85c
Saifur Rahman <saifur.rubayet@northsouth.edu>
2024-11-01 10:56:18 +0600

    modal added in card-details page

--- src/app/card-details/card-details.page.ts
+++ src/app/card-details/card-details.page.ts
@@ -1,5 +1,8 @@
 import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-card-details',
@@ -8,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardDetailsPage implements OnInit {
 
+  @ViewChild(IonModal) modal!: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string= "";
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+
   product = [
     {
       "name": "Pixel 7",
-      "description": "Lorem, ipsum dolor sit",
+      "description": "The Google Pixel 7 is a sleek smartphone with a 6.3-inch OLED display, known for its vibrant colors and smooth 90Hz refresh rate. Powered by Google’s custom Tensor G2 chip, it offers enhanced AI features, efficient performance, and excellent battery life.",
       "price": "52000",
       "img": ""
 
